2b8678b3d43a650abec42f1e246db87399c8348b
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-02-12 23:12:51 -0500

    Connect add page to firebase database

--- process_form.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    document.myform.addEventListener("submit", submitForm, false);
-}
-
-function submitForm(event) {
-    event.preventDefault();
-    alert("Function Coming Soon");
-}
-window.addEventListener("load", init, false);
